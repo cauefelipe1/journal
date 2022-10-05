e246f7a87a0d9b32539fd0f46074bb8e52c662b2
Caue Carvalho <cauefelipe1@hotmail.com>
2022-10-05 16:44:52 -0300

    Adding model year column into the vehicle table and creating a seed data script for DB version 6 to avoid mess in the identity columns.

--- Journal-Services/Migration/Journal.Migration/Migrations/00006_CreateDriverTables.cs
+++ Journal-Services/Migration/Journal.Migration/Migrations/00006_CreateDriverTables.cs
@@ -12,42 +12,32 @@ public class CreateDriverTables_00006 : BaseMigration {
     {
         InternalCreateDriverTable();
         InternalCreateVehicleTable();
-        PopulateVehicle();
+        SeedData();
     }
 
     private void InternalCreateDriverTable()
     {
         Create.Table("driver").InSchema(Settings.Database.SearchPath)
-            .WithColumn("driver_id").AsInt32().NotNullable().PrimaryKey().Identity(PostgresGenerationType.ByDefault)
+            .WithColumn("driver_id").AsInt32().NotNullable().PrimaryKey().Identity()
             .WithColumn("first_name").AsString(200).NotNullable()
             .WithColumn("last_name").AsString(100).NotNullable()
             .WithColumn("user_id")
-                .AsInt32()
-                .NotNullable()
-                .WithDefaultValue(0)
-                .WithColumnDescription($"It refers to the SecondaryId column on {Identity.Constants.IDENTITY_DB_SCHEMA}.app_user");
+            .AsInt32()
+            .NotNullable()
+            .WithDefaultValue(0)
+            .WithColumnDescription($"It refers to the SecondaryId column on {Identity.Constants.IDENTITY_DB_SCHEMA}.app_user");
 
-        Insert.IntoTable("driver").InSchema(Settings.Database.SearchPath)
-            .Row(new
-            {
-                driver_id = 1,
-                first_name = "Dominic",
-                last_name = "Toretto",
-                user_id = 1
-            })
-            .Row(new
-            {
-                driver_id = 2,
-                first_name = "Brian",
-                last_name = "O'Conner",
-                user_id = 2
-            });
+        Create.Index("idx_driver_user_id").OnTable("driver").InSchema(Settings.Database.SearchPath)
+            .OnColumn("user_id")
+            .Unique()
+            .WithOptions()
+                .NonClustered();
     }
 
     private void InternalCreateVehicleTable()
     {
         Create.Table("vehicle").InSchema(Settings.Database.SearchPath)
-            .WithColumn("vehicle_id").AsInt32().NotNullable().PrimaryKey().Identity(PostgresGenerationType.ByDefault)
+            .WithColumn("vehicle_id").AsInt32().NotNullable().PrimaryKey().Identity()
             .WithColumn("vehicle_secondary_id")
             .AsString()
             .Nullable()
@@ -57,6 +47,7 @@ public class CreateDriverTables_00006 : BaseMigration {
             .WithColumn("nickname").AsString(50).Nullable()
             .WithColumn("vehicle_type_id").AsInt16().NotNullable()
             .WithColumn("vehicle_brand_id").AsInt16().NotNullable()
+            .WithColumn("model_year").AsInt16().Nullable()
             .WithColumn("main_driver_id").AsInt32().NotNullable();
 
         Create.ForeignKey()
@@ -72,47 +63,11 @@ public class CreateDriverTables_00006 : BaseMigration {
             .ToTable("driver").InSchema(Settings.Database.SearchPath).PrimaryColumn("driver_id");
     }
 
-    private void PopulateVehicle()
+    private void SeedData()
     {
-        Insert.IntoTable("vehicle").InSchema(Settings.Database.SearchPath)
-            .WithOverridingSystemValue()
-            .Row(new
-            {
-                vehicle_id = 1,
-                model = "RX7",
-                nickname = null as string,
-                vehicle_type_id = 1, //Car
-                vehicle_brand_id = 5, //Mazda
-                main_driver_id = 1 //Dominic Toretto
-            })
-            .Row(new
-            {
-                vehicle_id = 2,
-                model = "Charger",
-                nickname = null as string,
-                vehicle_type_id = 1, //Car
-                vehicle_brand_id = 7, //Dodge
-                main_driver_id = 1 //Dominic Toretto
-            })
-            .Row(new
-            {
-                vehicle_id = 3,
-                model = "Supra",
-                nickname = null as string,
-                vehicle_type_id = 1, //Car
-                vehicle_brand_id = 6, //Toyota
-                main_driver_id = 2 //Brian O'Conner
-            })
-            .Row(new
-            {
-                vehicle_id = 4,
-                model = "Skyline",
-                nickname = null as string,
-                vehicle_type_id = 1, //Car
-                vehicle_brand_id = 8, //Nissan
-                main_driver_id = 2 //Brian O'Conner
-            });
-
+        Execute.Script(
+            $"Migrations/SqlScripts/{GetCurrentFileName()}.sql",
+            new Dictionary<string, string>() { { "Schema", Settings.Database.SearchPath } });
     }
 
     public override void Down()
--- Journal-Services/Migration/Journal.Migration/Migrations/BaseMigration.cs
+++ Journal-Services/Migration/Journal.Migration/Migrations/BaseMigration.cs
@@ -10,4 +10,15 @@ public abstract class BaseMigration: FluentMigrator.Migration
 
     protected IDictionary<string, object?> ToSnakeCase(object @object, params string[] columnsToSkip) =>
         SnakeCaseAdapter.ConvertToSnakeCase(@object, columnsToSkip);
+
+    /// <summary>
+    /// This method will work when the class name follows the following pattern: Part1_Part2.
+    /// </summary>
+    /// <returns>The class name inverted.</returns>
+    protected string GetCurrentFileName()
+    {
+        string[] className = GetType().Name.Split('_');
+
+        return $"{ className[1] }_{ className[0] }";
+    }
 }
