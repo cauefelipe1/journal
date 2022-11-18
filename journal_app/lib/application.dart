406dc6b0f21b58e9872e19c581568d6299160489
Caue Carvalho <cauefelipe1@hotmail.com>
2022-11-18 18:31:13 -0300

    Adding the riverpod in the mobile app

---
+++ journal_app/lib/application.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:journal_mobile_app/gui/identity/login_page.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class Application extends StatelessWidget {
+  const Application({Key? key}) : super(key: key);
+
+  static const String _title = 'Journal';
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
+      debugShowCheckedModeBanner: false,
+      theme: _getThemeData(),
+      home: const LoginPage(title: ""),
+    );
+  }
+
+  ThemeData _getThemeData() => ThemeData(
+        primarySwatch: Colors.teal,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+            shape: MaterialStatePropertyAll(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0),
+              ),
+            ),
+          ),
+        ),
+      );
+}
--- journal_app/lib/main.dart
+++ journal_app/lib/main.dart
@@ -2,9 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:journal_mobile_app/gui/identity/login_page.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:journal_mobile_app/application.dart';
 
 class MyHttpOverrides extends HttpOverrides {
   @override
@@ -22,38 +21,6 @@ void main() {
     HttpOverrides.global = MyHttpOverrides();
   }
 
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  static const String _title = 'Journal';
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: _title,
-      onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
-      localizationsDelegates: AppLocalizations.localizationsDelegates,
-      supportedLocales: AppLocalizations.supportedLocales,
-      debugShowCheckedModeBanner: false,
-      theme: _getThemeData(),
-      home: const LoginPage(title: ""),
-    );
-  }
-
-  ThemeData _getThemeData() => ThemeData(
-        primarySwatch: Colors.teal,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            shape: MaterialStatePropertyAll(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-            ),
-          ),
-        ),
-      );
+  //As the application is using the Riverpod, it must be wraped in a ProviderScope
+  runApp(const ProviderScope(child: Application()));
 }
