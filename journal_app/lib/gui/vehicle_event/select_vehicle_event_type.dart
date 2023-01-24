5dbefea18168999e73aa8dcd8b69f7f1e27e7962
Caue Carvalho <cauefelipe1@hotmail.com>
2023-01-24 11:44:11 -0300

    Mobile App Adding missing file

---
+++ journal_app/lib/gui/vehicle_event/select_vehicle_event_type.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SelectVehicleEventType extends StatelessWidget {
+  const SelectVehicleEventType({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Select the event type."),
+    );
+  }
+}
