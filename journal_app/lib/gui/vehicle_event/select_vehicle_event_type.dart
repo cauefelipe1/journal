da30ac1b2aae136dad3d027e68cf6050bd0b0a7c
Caue Carvalho <cauefelipe1@hotmail.com>
2023-05-18 16:22:28 -0300

    Mobile App Removing flutter_staggered_grid_view as it wasn't working as expected.

--- journal_app/lib/gui/vehicle_event/select_vehicle_event_type.dart
+++ journal_app/lib/gui/vehicle_event/select_vehicle_event_type.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:journal_mobile_app/l10n/app_localization_context.dart';
 import 'package:journal_mobile_app/models/vehicle_event.dart';
 
@@ -10,100 +9,86 @@ class SelectVehicleEventType extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[200],
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                context.l10n.chooseEventTypeTitle,
-                style: const TextStyle(
-                  fontSize: 35,
+      body: LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints viewportConstraints) {
+          return SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      context.l10n.chooseEventTypeTitle,
+                      style: const TextStyle(
+                        fontSize: 35,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    _getCard(context, VehicleEventType.refueling),
+                    _getCard(context, VehicleEventType.maintenance),
+                    _getCard(context, VehicleEventType.expense),
+                    _getCard(context, VehicleEventType.income),
+                    _getCard(context, VehicleEventType.route),
+                  ],
                 ),
               ),
-              SizedBox(height: 15),
-              StaggeredGrid.count(
-                crossAxisCount: 2,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-                children: [
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child: _getCard(context, VehicleEventType.refueling),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child: _getCard(context, VehicleEventType.maintenance),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child: _getCard(context, VehicleEventType.expense),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 1,
-                    mainAxisCellCount: 1,
-                    child: _getCard(context, VehicleEventType.income),
-                  ),
-                  StaggeredGridTile.count(
-                    crossAxisCellCount: 2,
-                    mainAxisCellCount: 1,
-                    child: _getCard(context, VehicleEventType.route),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
 
   Widget _getCard(BuildContext context, VehicleEventType type) {
-    return SizedBox(
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: InkWell(
-          onTap: () {
-            //onPressed?.call(vehicleId);
-          },
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: _getIndicatorColor(type),
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                  child: Icon(
-                    _getIndicatorIcon(type),
-                    size: 65,
-                    color: Colors.white,
+    return Row(
+      children: [
+        Expanded(
+          child: Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: InkWell(
+                onTap: () {
+                  //onPressed?.call(vehicleId);
+                },
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: _getIndicatorColor(type),
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        child: Icon(
+                          _getIndicatorIcon(type),
+                          size: 65,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        _getCardLabel(context, type),
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  _getCardLabel(context, type),
-                  style: const TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w500,
-                  ),
-                ),
-              ],
+              ),
             ),
           ),
         ),
-      ),
+      ],
     );
   }
 
