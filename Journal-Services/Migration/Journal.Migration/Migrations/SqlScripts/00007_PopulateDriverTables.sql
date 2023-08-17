619445f5ff6583cdbafbca8bd7a21c51597efa9f
Caue Carvalho <cauefelipe1@hotmail.com>
2023-08-17 18:32:17 -0300

    Adjusting and populating the secondary id field
    For Driver and Vehicle

--- Journal-Services/Migration/Journal.Migration/Migrations/SqlScripts/00007_PopulateDriverTables.sql
+++ Journal-Services/Migration/Journal.Migration/Migrations/SqlScripts/00007_PopulateDriverTables.sql
@@ -4,29 +4,29 @@ DO $$
     BEGIN
         /* BEGIN: Driver 1 */
         insert into $(Schema).driver
-            (first_name, last_name, country_id, user_id)
+            (secondary_id, first_name, last_name, country_id, user_id)
         values
-            ('Dominic', 'Toretto', 224, (select secondary_id from $(Identity_Schema).app_user where user_name like 'dominic'))
+            ('c6f65bc3-5b94-4a35-bde5-743dc648c8f2', 'Dominic', 'Toretto', 224, (select secondary_id from $(Identity_Schema).app_user where user_name like 'dominic'))
         returning driver_id into l_driver_id;
 
         insert into $(Schema).vehicle
             (secondary_id, model, nickname, vehicle_type_id, vehicle_brand_id, model_year, main_driver_id)
         values
-            (null, 'RX7', null, 1, 5 /*Mazda*/, 1997, 1),
-            (null, 'Charger', null, 1, 7 /*Dodge*/, 1970, 1);
+            ('770e4907-6119-4758-a542-3c90cf7bc96e', 'RX7', null, 1, 5 /*Mazda*/, 1997, 1),
+            ('1ee5f7f6-3bb3-4c7b-b1e6-d9bbbe89f738', 'Charger', null, 1, 7 /*Dodge*/, 1970, 1);
         /* END: Driver 1 */
 
         /* BEGIN: Driver 2 */
         insert into $(Schema).driver
-            (first_name, last_name, country_id, user_id)
+            (secondary_id, first_name, last_name, country_id, user_id)
         values
-            ('Brian', 'O''Conner', 224, (select secondary_id from $(Identity_Schema).app_user where user_name like 'brian'))
+            ('4930e98b-2664-4604-940e-3af77fe8c3fe', 'Brian', 'O''Conner', 224, (select secondary_id from $(Identity_Schema).app_user where user_name like 'brian'))
         returning driver_id into l_driver_id;
 
         insert into $(Schema).vehicle
             (secondary_id, model, nickname, vehicle_type_id, vehicle_brand_id, model_year, main_driver_id)
         values
-            (null, 'Supra', null, 1, 6 /*Toyota*/, 1997, 2),
-            (null, 'Skyline', null, 1, 8 /*Nissan*/, 1999, 2);
+            ('1fcf10f0-df1f-403d-a302-eb7d46caeac8', 'Supra', null, 1, 6 /*Toyota*/, 1997, 2),
+            ('b72ba6be-9fa4-4afd-b4b6-b4aabba2fb52', 'Skyline', null, 1, 8 /*Nissan*/, 1999, 2);
         /* END: Driver 2 */
     END $$
