ddb70e5c0c32f6780b61607376837bf0f98a87e8
Caue Carvalho <cauefelipe1@hotmail.com>
2023-08-26 14:25:28 -0300

    [API] - Story #35 - Add diver mobile controller

---
+++ Journal-Services/Application/Journal.API/Features/Driver/DriverMobileController.cs
@@ -0,0 +1,67 @@
+using Journal.API.Extensions;
+using Journal.Domain.Models.Driver;
+using Journal.Infrastructure.Features.Driver;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Journal.API.Features.Driver;
+
+/// <summary>
+/// Defines the controller to handle the driver endpoints.
+/// </summary>
+[ApiController]
+[Produces("application/json")]
+[Authorize]
+[Route("app/driver/mobile")]
+[ProducesResponseType(StatusCodes.Status200OK)]
+[ProducesResponseType(StatusCodes.Status400BadRequest)]
+public class DriverMobileController : ControllerBase
+{
+    private readonly ISender _sender;
+
+    /// <inheritdoc/>
+    public DriverMobileController(ISender sender) => _sender = sender;
+
+    /// <summary>
+    /// Gets a driver based on its ID.
+    /// </summary>
+    /// <param name="driverId">The driver unique identifier.</param>
+    /// <returns>An instance of <see cref="DriverModel"/></returns>
+    [HttpGet("{driverId:guid}")]
+    public async Task<ActionResult<DriverMobileModel>> GetDriverById(Guid driverId)
+    {
+        var driver = await _sender.Send(new DriverMediator.GetDriverByIdQuery(driverId));
+
+        if (driver is null)
+            return NotFound();
+
+        var result = DriverMobileModel.FromModel(driver);
+
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// Creates a new driver in the application.
+    /// </summary>
+    /// <param name="input">The input for creating a new driver.</param>
+    /// <returns>The ID of the driver saved.</returns>
+    [HttpPost]
+    public async Task<ActionResult<long>> CreateDriver(CreateDriverInput input)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        var model = new DriverModel
+        {
+            FirstName = input.FirstName,
+            LastName = input.LastName,
+            CountryId = input.CountryId,
+            UserId = this.GetUserSecondaryId()
+        };
+
+        long driverId = await _sender.Send(new DriverMediator.CreateDriverByIdQuery(model));
+
+        return Created(string.Empty, driverId);
+    }
+}
---
+++ Journal-Services/Application/Journal.API/Features/Driver/DriverModel.cs
@@ -0,0 +1,51 @@
+using Journal.Domain.Models.Driver;
+
+namespace Journal.API.Features.Driver;
+
+/// <summary>
+/// Represents a driver for the Mobile Application
+/// </summary>
+public class DriverMobileModel
+{
+    /// <see cref="DriverModel.SecondaryId"/>
+    /// <example>f58ae19d-8394-43de-9568-7bdfc745f643</example>
+    public Guid Id { get; set; }
+
+    /// <see cref="DriverModel.FirstName"/>
+    /// <example>Dominic</example>
+    public string FirstName { get; set; } = default!;
+
+    /// <see cref="DriverModel.LastName"/>
+    /// <example>Toretto</example>
+    public string LastName { get; set; } = default!;
+
+    /// <see cref="DriverModel.FullName"/>
+    /// <example>Dominic Toretto</example>
+    public string FullName { get; set; } = default!;
+
+    /// <see cref="DriverModel.CountryId"/>
+    /// <example>1</example>
+    public int CountryId { get; set; }
+
+    /// <see cref="DriverModel.UserId"/>
+    /// <example>1</example>
+    public int UserId { get; set; }
+
+    public static DriverMobileModel FromModel(DriverModel source)
+    {
+        if (source is null)
+            throw new ArgumentNullException(nameof(source));
+
+        var instance = new DriverMobileModel
+        {
+            Id = source.SecondaryId,
+            FirstName = source.FirstName,
+            LastName = source.LastName,
+            FullName = source.FullName,
+            CountryId = source.CountryId,
+            UserId = source.UserId
+        };
+
+        return instance;
+    }
+}
