477dd012a1b3a5204794a5cfb436ccb00d38c3e6
Caue Felipe Carvalho <cauefelipe1@hotmail.com>
2023-11-28 11:53:58 +0000

    Merged PR 3: General fixes and adding all http error handlers
    General fixes and adding all http error handlers
    
    Related work items: #46

--- journal_app/lib/infrastructure/http/error_hanlder/http_error_handler.dart
+++ journal_app/lib/infrastructure/http/error_hanlder/http_error_handler.dart
@@ -1,12 +1,21 @@
 import 'dart:io';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:journal_mobile_app/infrastructure/http/error_hanlder/http_error_handler_android.dart';
+import 'package:journal_mobile_app/infrastructure/http/error_hanlder/http_error_handler_ios.dart';
 import 'package:journal_mobile_app/infrastructure/http/error_hanlder/http_error_handler_macos.dart';
 import 'package:journal_mobile_app/infrastructure/http/http_error.dart';
 
 final httpErrorHandlerProvider = Provider<IHttpErrorHandler>((ref) {
   if (Platform.isMacOS) {
     return new HttpErrorHandlerMacOS();
+
+  } else if (Platform.isIOS){
+    return new HttpErrorHandlerIos();
+
+  } else if (Platform.isAndroid){
+    return new HttpErrorHandlerAndroid();
+    
   }
 
   throw Exception("Platform not supported.");
